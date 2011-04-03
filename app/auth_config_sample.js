eca02d07dbf180592e18f1e81091b0fe605993f1
Christian Wilcox <cw@christianwilcox.com>
2011-04-03 10:05:52 -0500

    added auth_config_sample.js and ignored auth_config.js

---
+++ app/auth_config_sample.js
@@ -0,0 +1,7 @@
+/*
+  Rename this file to auth_config.js 
+  and fill in the values below with your own
+  OAuth-approved key and secret.
+*/
+var IMGUR_CONSUMERKEY = '';
+var IMGUR_CONSUMERSECRET = '';
